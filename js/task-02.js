fcb10c0e89a5298e0b0043fd89f046d8053bacc4
M1keXl <mihayloh.com@gmail.com>
2023-03-06 22:55:36 +0200

    v1

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const name = ingredients[i];
+  const itemElm = document.createElement("li");
+  itemElm.classList.add("item");
+  itemElm.textContent = `${name}`;
+
+  const listElm = document.getElementById("ingredients");
+  listElm.append(itemElm);
+}
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,17 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const list = document.querySelector(".gallery");
+const newElement = images
+  .map(
+    (element) =>
+      `<li><img class="picture" src=${element.url} width = '350' alt='${element.alt}'></img></li>`
+  )
+  .join("");
+list.insertAdjacentHTML("beforeend", newElement);
+
+list.style.listStyle = "none";
+list.style.display = "flex";
+list.style.gap = "20px";
+list.style.justifyContent = "center";
+list.style.alignItems = "center";
